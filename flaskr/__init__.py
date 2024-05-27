9ee09b4397a684bca1ae321ba75efb374e23a5f8
voxten <voxterman@gmail.com>
2024-05-27 21:29:27 +0200

    Added Social Media scrapping, also blocked possibility for inserting duplicated values into a MongoDB

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,46 +1,56 @@
-from flask import Flask, jsonify, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for
 import os
-import pymongo
+from pymongo import MongoClient
 import asyncio
 import re
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 from pymongo.server_api import ServerApi
+from concurrent.futures import ProcessPoolExecutor
 
-# Flask app initialization
+# Inicjalizacja aplikacji Flask
 app = Flask(__name__)
 
-# Fetching MongoDB connection details from environment variables
+# Pobranie informacji dotyczących połączenia z bazą z zmiennych środowiskowych
 mongo_host = os.getenv('MONGO_HOST', 'localhost')
 mongo_port = os.getenv('MONGO_PORT', '27017')
 mongo_user = os.getenv('MONGO_INITDB_ROOT_USERNAME', 'root')
 mongo_pass = os.getenv('MONGO_INITDB_ROOT_PASSWORD', 'pass')
 mongo_url = f"mongodb://{mongo_user}:{mongo_pass}@{mongo_host}:{mongo_port}/"
 
-client = pymongo.MongoClient(mongo_url, server_api=ServerApi('1'))
+client = MongoClient(mongo_url, server_api=ServerApi('1'))
 
 mydb = client["Projekt"]
+scraped_urls_collection = mydb["scraped_urls"]
 
-# Send a ping to confirm a successful connection
+# Testowanie połączenia
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
 
-# Helper function to convert ObjectId to string
+
+# Konwertuje ObjectID do Stringa
 def serialize_document(doc):
     if '_id' in doc:
         doc['_id'] = str(doc['_id'])
     return doc
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/scrape', methods=['POST'])
 async def scrape():
     url = request.form['url']
+
+    # Sprawdź czy obecny url został już wcześniej zescrappowany
+    already_scraped = scraped_urls_collection.find_one({"url": url})
+
     html = await fetch_content(url)
 
     phone_number_patterns = [
@@ -52,67 +62,79 @@ async def scrape():
         r'\b\+\d{2} \d{3}-\d{3}-\d{3}\b'  # +xx xxx-xxx-xxx
     ]
 
-    tasks = [
-        parse_phone_numbers(html, phone_number_patterns),
-        parse_emails(html),
-        parse_street_names(html),
-        parse_cities(html)  # Parsing cities using city names file
-    ]
+    # Użycie ProcessPoolExecutor dla CPU-bound tasks
+    loop = asyncio.get_event_loop()
+    with ProcessPoolExecutor() as executor:
+        tasks = [
+            loop.run_in_executor(executor, parse_phone_numbers, html, phone_number_patterns),
+            loop.run_in_executor(executor, parse_emails, html),
+            loop.run_in_executor(executor, parse_street_names, html),
+            loop.run_in_executor(executor, parse_social_media, html, url)
+        ]
 
-    phone_numbers, emails, street_names, cities = await asyncio.gather(*tasks)
+        phone_numbers, emails, street_names, social_media = await asyncio.gather(*tasks)
 
     results = {
         "phone_numbers": phone_numbers if phone_numbers else [],
         "emails": emails if emails else [],
         "street_names": street_names if street_names else [],
-        "cities": cities if cities else []
+        "social_media": social_media if social_media else []
     }
 
-    # Create a new collection for the site
-    site_collection = mydb[url.replace("https://", "").replace("/", "_")]
+    if not already_scraped:
+        # Tworzenie kolekcji dla strony
+        site_collection = mydb[url.replace("https://", "").replace("/", "_")]
 
-    # Store scraped data in MongoDB with site information
-    if phone_numbers:
-        site_collection.insert_one({"type": "phone_numbers", "data": phone_numbers})
+        # Przechowywanie zescrappowanego kontentu
+        if phone_numbers:
+            site_collection.insert_one({"type": "phone_numbers", "data": phone_numbers})
 
-    if emails:
-        site_collection.insert_one({"type": "emails", "data": emails})
+        if emails:
+            site_collection.insert_one({"type": "emails", "data": emails})
 
-    if street_names:
-        site_collection.insert_one({"type": "street_names", "data": street_names})
+        if street_names:
+            site_collection.insert_one({"type": "street_names", "data": street_names})
 
-    if cities:
-        site_collection.insert_one({"type": "cities", "data": cities})
+        if social_media:
+            site_collection.insert_one({"type": "social_media", "data": social_media})
+
+        # Oznacz podany URL jako już zescrappowany
+        scraped_urls_collection.insert_one({"url": url})
 
     return render_template('results.html', url=url, results=results)
+
 
 @app.route('/all_data')
 def all_data():
     collections = mydb.list_collection_names()
     all_data = {}
     for collection_name in collections:
-        collection = mydb[collection_name]
-        documents = collection.find()
-        all_data[collection_name] = [serialize_document(doc) for doc in documents]
+        if collection_name != "scraped_urls":
+            collection = mydb[collection_name]
+            documents = collection.find()
+            all_data[collection_name] = [serialize_document(doc) for doc in documents]
 
     return render_template('all_data.html', all_data=all_data)
+
 
 @app.route('/delete_all', methods=['POST'])
 def delete_all():
     collections = mydb.list_collection_names()
     for collection_name in collections:
-        mydb[collection_name].drop()  # Drop the collection
+        mydb[collection_name].drop()  # Drop collection
     return redirect(url_for('all_data'))
 
-# Asynchronous functions for scraping and parsing
+
 async def fetch_content(url):
-    response = requests.get(url)
+    loop = asyncio.get_event_loop()
+    response = await loop.run_in_executor(None, requests.get, url)
     if response.status_code == 200:
         return response.text
     else:
         return None
 
-async def parse_phone_numbers(html, patterns):
+
+def parse_phone_numbers(html, patterns):
     if html:
         soup = BeautifulSoup(html, 'html.parser')
         phone_numbers = []
@@ -122,14 +144,16 @@ async def parse_phone_numbers(html, patterns):
     else:
         return None
 
-async def parse_emails(html):
+
+def parse_emails(html):
     if html:
         emails = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', html)
         return list(set(emails))
     else:
         return None
 
-async def parse_street_names(html):
+
+def parse_street_names(html):
     if html:
         soup = BeautifulSoup(html, 'html.parser')
         street_names = [tag.get_text() for tag in soup.find_all(string=re.compile(r'^ul\.', re.IGNORECASE))]
@@ -137,25 +161,30 @@ async def parse_street_names(html):
     else:
         return None
 
-async def parse_cities(html):
-    city_file = 'city_names.txt'
-    if html:
-        soup = BeautifulSoup(html, 'html.parser')
-        try:
-            with open(city_file, 'r') as f:
-                city_names = [line.strip() for line in f]
-        except FileNotFoundError:
-            print("City file not found.")
-            return None
 
-        cities = []
-        for city_name in city_names:
-            city_matches = soup.find_all(string=re.compile(r'\b{}\b'.format(re.escape(city_name)), re.IGNORECASE))
-            if city_matches:
-                cities.extend(city_matches)
-        return list(set(cities))  # Remove duplicates
+def parse_social_media(html, base_url):
+    if html:
+        social_media_patterns = {
+            "twitter": r'https?://(?:www\.)?twitter\.com/[A-Za-z0-9_]+',
+            "facebook": r'https?://(?:www\.)?facebook\.com/[A-Za-z0-9_.-]+',
+            "instagram": r'https?://(?:www\.)?instagram\.com/[A-Za-z0-9_.-]+',
+            "youtube": r'https?://(?:www\.)?youtube\.com/[A-Za-z0-9_./-]+'
+        }
+        soup = BeautifulSoup(html, 'html.parser')
+        social_media_links = {platform: [] for platform in social_media_patterns}
+
+        for a_tag in soup.find_all('a', href=True):
+            href = a_tag['href']
+            full_url = urljoin(base_url, href)
+
+            for platform, pattern in social_media_patterns.items():
+                if re.match(pattern, full_url):
+                    social_media_links[platform].append(full_url)
+
+        return {platform: list(set(links)) for platform, links in social_media_links.items() if links}
     else:
         return None
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
